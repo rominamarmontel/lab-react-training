bb2bae90376d8183fe10967d081bbbadd7a4c213
romi <romi.varnier@gmail.com>
2023-02-28 13:56:15 +0100

    Iteration 1 | Component: IdCard Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import IdCard from './components/IdCard';
+
+const usersArray = [
+  {
+    lastName: 'Doe',
+    firstName: 'John',
+    gender: 'male',
+    height: 178,
+    birth: new Date('1992-07-14'),
+    picture: 'https://randomuser.me/api/portraits/men/44.jpg',
+  },
+  {
+    lastName: 'Delores',
+    firstName: 'Obrien',
+    gender: 'female',
+    height: 172,
+    birth: new Date('1988-05-11'),
+    picture: 'https://randomuser.me/api/portraits/women/44.jpg',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {usersArray.map((user) => {
+        return <IdCard id={user} />;
+      })}
     </div>
   );
 }
 
 export default App;
+
+// return (
+//   <section>
+//     <Counter />
+//     <ClassCounter />
+//     <Container>
+//       {movies.map((movie) => {
+//         return (
+//           <div className="CardMovie">
+//             <h3>{movie.title}</h3>
+//             <p>Director: {movie.director}</p>
+//             <p>Production year: {movie.year}</p>
+//             <p>Genres: {movie.genre}</p>
+//             <p>Score: {movie.score}</p>
+//           </div>
+//         );
+//       })}
+//     </Container>
+//   </section>
+// );
---
+++ src/components/IdCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './IdCard.css';
+
+const IdCard = ({ id }) => {
+  return (
+    <div class="IdCard">
+      <div class="card">
+        <img src={id.picture} alt="" />
+        <div class="container">
+          <p class="lastName">
+            <span>Last name: </span>
+            {id.lastName}
+          </p>
+          <p class="firstName">
+            <span>First name: </span>
+            {id.firstName}
+          </p>
+          <p class="gender">
+            <span>Gender: </span>
+            {id.gender}
+          </p>
+          <p class="height">
+            <span>Height: </span>
+            {id.height}
+          </p>
+          <p class="birth">
+            <span>Birth: </span>
+            {id.birth.toString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IdCard;
